5359d235dca8fa41422e49b8333026d195093fd5
cjgrady <cjgrady@gmail.com>
2022-04-26 11:16:21 -0500

    Add myst_parser extension to sphinx configuration

--- _sphinx_config/conf.py
+++ _sphinx_config/conf.py
@@ -43,6 +43,7 @@ extensions = [
     #'sphinx.ext.napoleon',
     'sphinx_rtd_theme',
     #'autoapi.extension',
+    'myst_parser',  # For MD support
 ]
 
 # Add any paths that contain templates here, relative to this directory.
