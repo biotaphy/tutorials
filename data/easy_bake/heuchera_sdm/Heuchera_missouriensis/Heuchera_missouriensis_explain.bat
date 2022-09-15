427280510745f9824ec70c16d0efd5202df1c5f5
zzeppozz <zzeppozz@gmail.com>
2022-09-15 14:20:36 -0500

    input data

---
+++ data/easy_bake/heuchera_sdm/Heuchera_missouriensis/Heuchera_missouriensis_explain.bat
@@ -0,0 +1,1 @@
+java -mx30688m -cp "/home/astewart/git/Maxent/ArchivedReleases/3.4.4/maxent.jar:/git/Maxent/ArchivedReleases/3.4.4/maxent.jar" density.Explain -l /volumes/output/heuchera_sdm/Heuchera_missouriensis/Heuchera_missouriensis.lambdas -c /volumes/output/heuchera_sdm/Heuchera_missouriensis/Heuchera_missouriensis.asc /volumes/env/worldclim1.4
