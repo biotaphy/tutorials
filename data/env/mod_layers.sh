2c5d3490e8b0d5fc1f6f31ed32f5f216791aaaef
zzeppozz <zzeppozz@gmail.com>
2022-10-31 14:10:50 -0500

    new env layers

---
+++ data/env/mod_layers.sh
@@ -0,0 +1,28 @@
+
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/BIOCLIM_1.tif   ryan_2.5_na/BIOCLIM_1.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/BIOCLIM_7.tif   ryan_2.5_na/BIOCLIM_7.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/BIOCLIM_12.tif   ryan_2.5_na/BIOCLIM_12.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/BIOCLIM_17.tif   ryan_2.5_na/BIOCLIM_17.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/GTOPO30_ELEVATION.tif   ryan_2.5_na/GTOPO30_ELEVATION.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/GTOPO30_SLOPE_reduced.tif   ryan_2.5_na/GTOPO30_SLOPE_reduced.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/ISRICSOILGRIDS_new_average_coarsefragmentpercent_reduced.tif   ryan_2.5_na/ISRICSOILGRIDS_new_average_coarsefragmentpercent_reduced.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/ISRICSOILGRIDS_new_average_phx10percent_reduced.tif   ryan_2.5_na/ISRICSOILGRIDS_new_average_phx10percent_reduced.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/ISRICSOILGRIDS_new_average_sandpercent_reduced.tif   ryan_2.5_na/ISRICSOILGRIDS_new_average_sandpercent_reduced.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/ISRICSOILGRIDS_new_average_soilorganiccarboncontent_reduced.tif   ryan_2.5_na/ISRICSOILGRIDS_new_average_soilorganiccarboncontent_reduced.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/LandCover_1_Needleleaf.tif   ryan_2.5_na/LandCover_1_Needleleaf.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/LandCover_6_Herbaceous.tif   ryan_2.5_na/LandCover_6_Herbaceous.tif
+gdal_translate -projwin  -164  64  -53  15 ryan_2.5_global/ecoreg_global_2.5min.tif  ryan_2.5_na/ecoreg_na_2.5min.tif
+
+
+gdal_translate  ryan_2.5_na/BIOCLIM_1.tif  ryan_2.5_na/BIOCLIM_1.asc
+gdal_translate  ryan_2.5_na/BIOCLIM_7.tif  ryan_2.5_na/BIOCLIM_7.asc
+gdal_translate  ryan_2.5_na/BIOCLIM_12.tif  ryan_2.5_na/BIOCLIM_12.asc
+gdal_translate  ryan_2.5_na/BIOCLIM_17.tif  ryan_2.5_na/BIOCLIM_17.asc
+gdal_translate  ryan_2.5_na/GTOPO30_ELEVATION.tif  ryan_2.5_na/GTOPO30_ELEVATION.asc
+gdal_translate  ryan_2.5_na/GTOPO30_SLOPE_reduced.tif  ryan_2.5_na/GTOPO30_SLOPE_reduced.asc
+gdal_translate  ryan_2.5_na/ISRICSOILGRIDS_new_average_coarsefragmentpercent_reduced.tif  ryan_2.5_na/ISRICSOILGRIDS_new_average_coarsefragmentpercent_reduced.asc
+gdal_translate  ryan_2.5_na/ISRICSOILGRIDS_new_average_phx10percent_reduced.tif  ryan_2.5_na/ISRICSOILGRIDS_new_average_phx10percent_reduced.asc
+gdal_translate  ryan_2.5_na/ISRICSOILGRIDS_new_average_sandpercent_reduced.tif  ryan_2.5_na/ISRICSOILGRIDS_new_average_sandpercent_reduced.asc
+gdal_translate  ryan_2.5_na/ISRICSOILGRIDS_new_average_soilorganiccarboncontent_reduced.tif  ryan_2.5_na/ISRICSOILGRIDS_new_average_soilorganiccarboncontent_reduced.asc
+gdal_translate  ryan_2.5_na/LandCover_1_Needleleaf.tif  ryan_2.5_na/LandCover_1_Needleleaf.asc
+gdal_translate  ryan_2.5_na/LandCover_6_Herbaceous.tif  ryan_2.5_na/LandCover_6_Herbaceous.asc
