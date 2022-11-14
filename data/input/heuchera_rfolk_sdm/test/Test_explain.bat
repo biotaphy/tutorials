35a4e1c219d51f9a8bc49660024f0376972735f2
zzeppozz <zzeppozz@gmail.com>
2022-11-14 16:41:00 -0600

    rename species layers for PAM to match tree

--- data/input/heuchera_rfolk_sdm/test/Test_explain.bat
+++
@@ -1,1 +0,0 @@
-java -mx30688m -cp "/git/Maxent/ArchivedReleases/3.4.4/maxent.jar:/git/Maxent/ArchivedReleases/3.4.4/maxent.jar" density.Explain -l /volumes/output/heuchera_rfolk_sdm/test/Test.lambdas -c /volumes/output/heuchera_rfolk_sdm/test/Test.asc /volumes/env/na_2.5min
